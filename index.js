7885628712e2a53bc0c6b2da663f6fa4b1aee59a
Nene <133941254+NeneShallCode@users.noreply.github.com>
2023-06-08 20:01:54 -0700

    made a better search engine

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20
-  }
-};
-function search(event){
+function search(event) {
   event.preventDefault();
-  let searchInput = document.querySelector ("#searchCity")
-  function showPassword(event) {
-  event.preventDefault();
-  let passwordInput = document.querySelector("#password-input");
-  alert(`Your password is ${passwordInput.value}`);
+  let city = document.querySelector("#searchCity").value;
+  searchCity(city);
 }
-let passwordForm = document.querySelector("#password-form");
-passwordForm.addEventListener("submit", showPassword);
+let date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let day = date.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let currentDay = days[day];
+let formattedDate = `${currentDay}, ${hours}:${minutes}`;
+let h3 = document.querySelector("h3");
+h3.innerHTML = formattedDate;
+
+function showTemp(response) {
+  document.querySelector(".city").innerHTML = response.data.name;
+  document.querySelector(".temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
 }
-let form = document.querySelector ("#searchEngine")
-form.addEventListener("submit", search)
+function searchCity(city) {
+  let units = "metric";
+  let apiKey = "514a1ffade9078bc9c2d40d114f61a0b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showTemp);
+}
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function searchLocate(position) {
+  let apiKey = "514a1ffade9078bc9c2d40d114f61a0b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+let searchForm = document.querySelector(".searchEngine");
+searchForm.addEventListener("submit", search);
